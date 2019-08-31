ad5ee8e93b4dee4814205c6317be2e6434c40415
Pablo Villacanas <pablo.villacanas@gmail.com>
2019-08-31 03:23:55 +0200

    Improve logic for management of git repositories

--- autoscript.sh
+++ autoscript.sh
@@ -6,7 +6,6 @@
 
 #------------GLOBAL_VARS & CONSTANTS-----------
 readonly DIST=$(echo $(uname -v) | cut -d" " -f3)
-readonly BASH_FILE=$([[ "$DIST" == "Debian" ]] && echo "$HOME/.bash_profiles" || echo "$HOME/.bashrc")
 
 #--------------------USER----------------------
 printf "%b" "You are $BGreen$(whoami)$Coloroff under $BYellow$DIST$Coloroff distribution are you sure yo want to continue? y/n "
@@ -25,17 +24,16 @@ sudo chmod +x ./dependencies/apt/dependencies.sh
 ./dependencies/git/dependencies.sh
 
 #---------------------ZSH----------------------
-# Configure zsh via oh my zsh!
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-# Spaceship plugin & others
-git clone https://github.com/denysdovhan/spaceship-prompt.git $ZSH_CUSTOM/themes/spaceship-prompt
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-
+if [[ -z $ZSH ]]; then
+	sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+	git clone https://github.com/denysdovhan/spaceship-prompt.git $ZSH_CUSTOM/themes/spaceship-prompt
+	ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+	chsh -s $(which zsh)
+else
+	upgrade_oh_my_zsh
+fi
 cp terminal/.zshrc $HOME/.zshrc
 cp terminal/oh-my-zsh.sh $ZSH
-# Set ZSH as default shell
-chsh -s $(which zsh)
-sed -ri "/^${USER}/s@/bin/bash@/usr/bin/zsh@g" /etc/passwd
 
 #---------------------TMUX---------------------
 cp configfiles/.tmux.conf $HOME/.tmux.conf
--- dependencies/git/dependencies.sh
+++ dependencies/git/dependencies.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 . utils/fileutils.sh
+. utils/systemutils.sh
 . versions
+
+api_repos="https://api.github.com/repos/"
 
 <<DOWNLOAD_REPO_VERSION
 This method updates a repository at above specified version if it is not yet  
@@ -10,10 +13,10 @@ This method updates a repository at above specified version if it is not yet
     $3-Version tag
 DOWNLOAD_REPO_VERSION
 download_repo_version() {
-    repo=`echo $git | sed -En 's/.*\.com\/([a-z]?)/\1/p' | cut -d '.' -f 1`
+    repo=$(curl -s "$api_repos$1" | grep -oP '"clone_url": "\K(.*)(?=")')
     if [ ! -f $2 ]; then
         mkdir $2
-        git clone $1 $2 --branch $3
+        git clone repo --branch $3 --depth 1
         echo "$repo repository cloned in $2"
         return
     else
@@ -25,7 +28,24 @@ download_repo_version() {
     fi
 }
 
+<<DOWNLOAD_LATEST_REPO_VERSION
+This method updates a repository to the last release version
+	$1-Repository url (absolute)
+    $2-Local repository path
+DOWNLOAD_LATEST_REPO_VERSION
+download_latest_repo_version() {
+    version=$(curl -s "$api_repos$1/releases/latest" | grep -oP '"tag_name": "\K(.*)(?=")')
+    git -C $2 pull $version
+}
+
 #asdf
-download_repo_version https://github.com/asdf-vm/asdf.git $HOME/.asdf $asdf_v
-append_to_file_if_not_exists ". $HOME/.asdf/asdf.sh" $HOME/.zshrc "Import source code of asd to zsh"
-append_to_file_if_not_exists $HOME/.asdf/completions/asdf.bash' >>~/.zshrc
+if [ command_not_exists asdf ]; then
+    download_latest_repo_version "$asdf_repo" $HOME/.asdf $asdf_v
+    append_to_file_if_not_exists "$HOME/.asdf/asdf.sh" $HOME/.zshrc "Import source functions of asdf to zsh"
+    append_to_file_if_not_exists "$HOME/.asdf/completions/asdf.bash" "Allow completion of asdf in zsh" >>~/.zshrc
+fi
+
+#spaceship plugin
+if ! [ -d $ZSH_CUSTOM/themes/spaceship-prompt ]; then
+    download_latest_repo_version $spaceship_repo $ZSH_CUSTOM/themes/spaceship-prompt
+fi
