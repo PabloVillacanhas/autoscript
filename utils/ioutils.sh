cc04b41442a5c2ac2ab93709022a66bb53f7a9fb
Pablo Villacanas <pablo.villacanas@gmail.com>
2019-09-08 11:08:32 +0200

    Fully merged previous branch

--- utils/ioutils.sh
+++ utils/ioutils.sh
@@ -1,3 +1,5 @@
+. $AUTOSCRIPT_PATH/resources/colors
+
 #Abstraction of queries about perform actions
 ask_confirmation() {
     echo "$2"
@@ -7,4 +9,17 @@ ask_confirmation() {
     else
         return 0
     fi
+}
+
+<<REQUEST_CONFIRMATION
+Request confirmation.
+$1-Sentence before confirmation
+Returns
+0-Confirmation aproved
+1-COnfirmation ddenied
+REQUEST_CONFIRMATION
+request_confirmation(){
+	printf "%b" "$1"
+	read response
+	echo "$response"
 }
---
+++ utils/prueba.sh
@@ -0,0 +1,3 @@
+. $AUTOSCRIPT_PATH/utils/ioutils.sh
+
+request_confirmation "Hello $BGreen$(whoami)$Coloroff "
