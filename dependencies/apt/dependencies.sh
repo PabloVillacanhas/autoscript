c7f5914ddf0f8166ad276723de02b51bbbedd59c
Pablo Villacañas <pablo.villacanas@gmail.com>
2019-09-15 22:29:59 +0200

    Starting to create yarn installation

--- dependencies/apt/dependencies.sh
+++ dependencies/apt/dependencies.sh
@@ -19,3 +19,12 @@ if [[ $response =~ ^[yY][eE][sS]|[yY]+$ ]]; then
 else
 	echo "Avoiding installing dependencies"
 fi
+
+install_yarn(){
+	sudo apt remove cmdtest
+	sudo apt remove yarn
+	curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+	echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+	sudo apt update
+	sudo apt install yarn
+}
