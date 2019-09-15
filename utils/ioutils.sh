2f223204e437f4b172fbbebcac9f8995046c64c4
Pablo Villacañas <pablo.villacanas@gmail.com>
2019-09-15 11:18:38 +0200

    Resolved conflicts
    Resolved conflicts

--- utils/ioutils.sh
+++ utils/ioutils.sh
@@ -1,33 +1,13 @@
-<<<<<<< Updated upstream
-#!/bin/bash
-
 . $AUTOSCRIPT_PATH/resources/colors
-
-=======
-. $AUTOSCRIPT_PATH/resources/colors
->>>>>>> Stashed changes
 <<REQUEST_CONFIRMATION
 Request confirmation.
 $1-Sentence before confirmation
 Returns
 0-Confirmation aproved
-<<<<<<< Updated upstream
-1-Confirmation denied
-REQUEST_CONFIRMATION
-request_confirmation(){
-	printf "%b" "$1 [Y/n]"
-	read res
-	if [[ $res =~ [Yy][Ee][Ss] || $res =~ ^[Yy]$ || -z $res ]]; then
-		return 0
-	else
-		return 1
-	fi
-=======
 1-COnfirmation ddenied
 REQUEST_CONFIRMATION
 request_confirmation(){
 	printf "%b" "$1"
 	read response
 	echo "$response"
->>>>>>> Stashed changes
 }
--- utils/prueba.sh
+++ utils/prueba.sh
@@ -1,17 +1,3 @@
-<<<<<<< Updated upstream
-#!/bin/bash
-
-. $AUTOSCRIPT_PATH/utils/ioutils.sh
-
-request_confirmation "Hello $BGreen$(whoami)$Coloroff"
-
-if [[ $? == 0 ]];then
-	echo "Confirmed"
-else
-	echo "Not confirmed"
-if
-=======
 . $AUTOSCRIPT_PATH/utils/ioutils.sh
 
 request_confirmation "Hello $BGreen$(whoami)$Coloroff "
->>>>>>> Stashed changes
