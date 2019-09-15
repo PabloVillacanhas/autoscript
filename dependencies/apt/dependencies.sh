f8f587390487e56100cc7e1fafa1340fd5e4186f
Pablo Villacañas <pablo.villacanas@gmail.com>
2019-09-15 22:32:38 +0200

    Changing yarn install via npm

--- dependencies/apt/dependencies.sh
+++ dependencies/apt/dependencies.sh
@@ -20,11 +20,3 @@ else
 	echo "Avoiding installing dependencies"
 fi
 
-install_yarn(){
-	sudo apt remove cmdtest
-	sudo apt remove yarn
-	curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-	echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-	sudo apt update
-	sudo apt install yarn
-}
