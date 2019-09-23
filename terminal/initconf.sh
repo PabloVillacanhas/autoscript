b471ab5e2ca9eed5e1ae83d8d9927866cd72708b
Pablo Villacanas <pablo.villacanas@gmail.com>
2019-09-23 16:59:22 +0200

    Change source by . in zsh script

--- terminal/initconf.sh
+++ terminal/initconf.sh
@@ -4,6 +4,6 @@ git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zs
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &> /dev/null
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm &> /dev/null
 cp $AUTOSCRIPT_PATH/terminal/zshrc $HOME/.zshrc
-source $HOME/.zshrc
+. $HOME/.zshrc
 cp $AUTOSCRIPT_PATH/terminal/oh-my-zsh.sh $ZSH
-source $ZSH/oh-my-zsh.sh
+. $ZSH/oh-my-zsh.sh
