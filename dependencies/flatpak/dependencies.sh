ae74e2348b92095ce038db495aa1641f4e4b9597
Pablo Villacanas <pablo.villacanas@gmail.com>
2019-09-26 18:25:00 +0200

    Install some apps from flathub

--- dependencies/flatpak/dependencies.sh
+++ dependencies/flatpak/dependencies.sh
@@ -1,1 +1,8 @@
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+#!/bin/bash
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo flatpak update
+sudo flatpak install -y flathub io.dbeaver.DBeaverCommunity 
+sudo flatpak install -y flathub com.visualstudio.code
+sudo flatpak install -y flathub com.getpostman.Postman
+sudo flatpak install -y flathub org.libreoffice.LibreOffice
+sudo flatpak install -y flathub com.axosoft.GitKraken
