c16bbd0a1356645faa7e64241240d1d28f26ea20
Pablo Villacanas <pablo.villacanas@gmail.com>
2019-10-13 00:05:51 +0200

    Installed p10k

--- terminal/initconf.sh
+++ terminal/initconf.sh
@@ -3,7 +3,10 @@
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions &> /dev/null
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &> /dev/null
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm &> /dev/null
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k &> /dev/null
+cp p10k ~/.p10k.zsh
 cp $AUTOSCRIPT_PATH/terminal/zshrc $HOME/.zshrc
 . $HOME/.zshrc
 cp $AUTOSCRIPT_PATH/terminal/oh-my-zsh.sh $ZSH/oh-my-zsh.sh
 . $ZSH/oh-my-zsh.sh
+
