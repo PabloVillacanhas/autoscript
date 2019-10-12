d94fc3e7a789b13cdbeb3b0800f055e797247bc6
Pablo Villacanas <pablo.villacanas@gmail.com>
2019-10-12 13:15:42 +0200

    Added alias vscode to provide cli operations from flatpack

--- dependencies/flatpak/dependencies.sh
+++ dependencies/flatpak/dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-sudo flatpak update
+sudo flatpak update -y
 sudo flatpak install -y flathub io.dbeaver.DBeaverCommunity 
 sudo flatpak install -y flathub com.visualstudio.code
 #sudo flatpak install -y flathub com.getpostman.Postman
