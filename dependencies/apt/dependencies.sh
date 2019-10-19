51e958c6b3422c2c986234253e634890df7a0d54
Pablo Villacanas <pablo.villacanas@gmail.com>
2019-10-19 20:58:53 +0200

    Install vscode by apt

--- dependencies/apt/dependencies.sh
+++ dependencies/apt/dependencies.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
 sudo apt update && sudo apt upgrade -y
 
 sudo apt install -y \
 	git \
+	curl \
+	apt-transport-https \
 	tmux \
 	xclip \
 	vim \
@@ -11,7 +18,8 @@ sudo apt install -y \
 	golang \
 	fzf \
 	postgresql-client-* \
-	flatpak
+	flatpak \
+	code
 
 sudo apt autoremove -y
 
