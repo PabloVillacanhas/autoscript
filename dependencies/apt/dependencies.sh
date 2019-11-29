c9d7fe4c62ba17c92dae854f4f08b0b2ec44aab4
Pablo Villacanas <pablo.villacanas@gmail.com>
2019-11-29 21:32:06 +0100

    Install virtualbox 6.0

--- dependencies/apt/dependencies.sh
+++ dependencies/apt/dependencies.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "deb http://download.virtualbox.org/virtualbox/debian stretch contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+
+
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
@@ -22,7 +27,9 @@ sudo apt install -y \
 	code \
 	openvpn \
 	traceroute \
-	pulseaudio pulseaudio-module-bluetooth pavucontrol bluez-firmware
+	pulseaudio pulseaudio-module-bluetooth pavucontrol bluez-firmware \
+	libcurl3 virtualbox-6.0
+
 
 
 sudo apt autoremove -y
